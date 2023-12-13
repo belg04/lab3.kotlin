888cfc61e31fd7c5dc352cf5cddeb3e4eaff92bd
belg04 <113061405+belg04@users.noreply.github.com>
2023-12-13 11:34:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.lab3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, NewActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
